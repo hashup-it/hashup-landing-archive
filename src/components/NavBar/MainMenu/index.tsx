b5e4d82300d42dafaa0c672b7ee10a8dd7c4c67b
Robert Dziubek <rdziubek@hashup.it>
2021-10-22 01:18:08 +0200

    Styling fixups (#11)

--- src/components/NavBar/MainMenu/index.tsx
+++ src/components/NavBar/MainMenu/index.tsx
@@ -1,19 +1,25 @@
-import { Icon } from "../index.styles"
-import { MenuItem, StyledMainMenu } from "./index.styles"
+import { MenuItem, StyledMainMenu } from './index.styles'
+import { Hyperlink } from '../../Shared/sections.styles'
 
 export const MainMenu = () => {
     return (
         <StyledMainMenu>
             <MenuItem>
-                Ecosystem
-                <Icon src='/assets/icons/chevron-down.svg'/>
+                <Hyperlink href="#ecosystem">
+                    Ecosystem
+                </Hyperlink>
+                {/*<Icon src='/assets/icons/chevron-down.svg'/>*/}
             </MenuItem>
-            <MenuItem>Team</MenuItem>
-            <MenuItem>Media</MenuItem>
-            <MenuItem>Investment</MenuItem>
+            {/*<MenuItem>Team</MenuItem>*/}
             <MenuItem>
-                Docs
-                <Icon src='/assets/icons/chevron-down.svg'/>
+                <Hyperlink href="https://www.linkedin.com/company/hashupit">Media</Hyperlink>
+            </MenuItem>
+            {/*<MenuItem>Investment</MenuItem>*/}
+            <MenuItem>
+                <Hyperlink href="https://hashup-it.gitbook.io/hashup-it-1/">
+                    Docs
+                </Hyperlink>
+                {/*<Icon src='/assets/icons/chevron-down.svg'/>*/}
             </MenuItem>
         </StyledMainMenu>
     )
