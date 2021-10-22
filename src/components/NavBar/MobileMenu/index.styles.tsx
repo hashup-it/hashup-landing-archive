a44f065ad1dc231d6de47035f2d05a1bd1829fcd
Robert Dziubek <rdziubek@hashup.it>
2021-10-22 13:42:29 +0200

    Implement mobile menu view

--- src/components/NavBar/MobileMenu/index.styles.tsx
+++ src/components/NavBar/MobileMenu/index.styles.tsx
@@ -3,6 +3,7 @@ import { Swatches } from "../../Swatches";
 
 export const StyledMobileMenu = styled.div<({opened : boolean})>`
     position: fixed;
+    overflow-y: scroll;
     background-color: ${Swatches.background_main};
     height: 100vh;
     width: 100vw;
@@ -10,13 +11,11 @@ export const StyledMobileMenu = styled.div<({opened : boolean})>`
     left: 0;
     top: 0;
     padding-top: 140px;
-
+    
     -webkit-transition: opacity 1s ease-in-out;
     -moz-transition: opacity 1s ease-in-out;
     -ms-transition: opacity 1s ease-in-out;
     -o-transition: opacity 1s ease-in-out;
     transition: opacity 1s ease-in-out;
     opacity: ${props => props.opened ? 1 : 0};
-
-
 `;
