01872f0e3effa4fdecccfee1571e5a741ab18655
Robert Dziubek <rdziubek@hashup.it>
2021-10-22 16:20:51 +0200

    Update gameexplorer component view

--- src/components/GamExplorer/index.styles.tsx
+++ src/components/GamExplorer/index.styles.tsx
@@ -4,13 +4,11 @@ import { SectionContainer } from '../Shared/sections.styles'
 export const StyledGamExplorer = styled.div`
     display: flex;
     justify-content: center;
-    
-    width: 100%;
 `;
 
 export const GameExplorerContainer = styled(SectionContainer)`
     display: flex;
-    width: 40%;
+    width: 30em;
     
     text-align: center;
 `
