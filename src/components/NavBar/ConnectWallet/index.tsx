4e4ad16d030a64f6667ce13b52eb7c862b12a789
Robert Dziubek <rdziubek@hashup.it>
2021-10-27 16:19:58 +0200

    Add cartrdiges view background haze

--- src/components/NavBar/ConnectWallet/index.tsx
+++ src/components/NavBar/ConnectWallet/index.tsx
@@ -6,35 +6,38 @@ import {
     WalletHideWrapper,
     WalletIcon
 } from './index.styles'
-import { FunctionComponent, useState } from 'react'
+import { FunctionComponent } from 'react'
 import { WalletSelector } from '../../WalletSelector'
 
 interface ConnectWalletProps {
     account: string | any,
-    handleMetamaskConnection: Function
+    handleMetamaskConnection: Function,
+    isWalletSelectorShown: boolean,
+    setIsWalletSelectorShown: Function
 }
 
-export const ConnectWallet: FunctionComponent<ConnectWalletProps> = ({ handleMetamaskConnection, account }) => {
-    const [isWalletSelectorShown, setIsWalletSelectorShown] = useState(false)
-
-
-    return (
-        <WalletHideWrapper>
-            <StyledConnectWallet onClick={() => setIsWalletSelectorShown(true)}>
-                <StyledWalletTextWrapper>
-                    <StyledWrapper>{account ? 'Connected' : 'Connect Wallet'}</StyledWrapper>
-                    {account && <StyledAddress>{account.slice(0, 4) + '...' + account.slice(37, 42)}</StyledAddress>}
-                </StyledWalletTextWrapper>
-                <WalletIcon src="/assets/icons/wallet.svg" />
-            </StyledConnectWallet>
-            {
-                isWalletSelectorShown &&
-                <WalletSelector
-                    setIsWalletSelectorShown={setIsWalletSelectorShown}
-                    handleMetamaskConnection={handleMetamaskConnection}
-                />
-            }
-        </WalletHideWrapper>
-
-    )
-}
+export const ConnectWallet: FunctionComponent<ConnectWalletProps> = (
+    {
+        handleMetamaskConnection,
+        account,
+        isWalletSelectorShown,
+        setIsWalletSelectorShown
+    }
+) => (
+    <WalletHideWrapper>
+        <StyledConnectWallet onClick={() => setIsWalletSelectorShown(true)}>
+            <StyledWalletTextWrapper>
+                <StyledWrapper>{account ? 'Connected' : 'Connect Wallet'}</StyledWrapper>
+                {account && <StyledAddress>{account.slice(0, 4) + '...' + account.slice(37, 42)}</StyledAddress>}
+            </StyledWalletTextWrapper>
+            <WalletIcon src="/assets/icons/wallet.svg" />
+        </StyledConnectWallet>
+        {
+            isWalletSelectorShown &&
+            <WalletSelector
+                setIsWalletSelectorShown={setIsWalletSelectorShown}
+                handleMetamaskConnection={handleMetamaskConnection}
+            />
+        }
+    </WalletHideWrapper>
+)
