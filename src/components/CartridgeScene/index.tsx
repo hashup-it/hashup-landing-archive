2b7a42177a6eab03f9d521790686a1688be6a37d
Robert Dziubek <rdziubek@hashup.it>
2021-10-29 14:33:02 +0200

    Update cartrdige models

--- src/components/CartridgeScene/index.tsx
+++ src/components/CartridgeScene/index.tsx
@@ -1,20 +1,22 @@
 import React, { Suspense } from 'react'
 import { Canvas, extend } from '@react-three/fiber'
-import Model from './Three/Cartridge'
+import Model from './Three/Gold'
 import { OrbitControls } from '@react-three/drei'
 
 extend({ OrbitControls })
 
 export enum CameraDistance {
-    mainLanding = -6,
-    cartridgesListing = -3.75
+    mainLanding = -20,
+    cartridgesListing = -18
 }
 
 export const CartridgeScene = (
     {
-        cameraDistance
+        cameraDistance,
+        cartridgeModel
     }: {
-        cameraDistance: CameraDistance
+        cameraDistance: CameraDistance,
+        cartridgeModel?: any
     }
 ) => (
     <Canvas
@@ -42,8 +44,12 @@ export const CartridgeScene = (
             <pointLight position={[-200, 10, -200]} intensity={15} />
             <pointLight position={[400, 10, 400]} intensity={.5} />
             <OrbitControls
+                enableZoom={false}
+                enablePan={false}
+                autoRotate
+                autoRotateSpeed={-.5}
             />
-            <Model />
+            {cartridgeModel ? cartridgeModel : <Model />}
         </Suspense>
     </Canvas>
 )
