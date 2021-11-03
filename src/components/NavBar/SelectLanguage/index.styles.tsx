bd4b4578856062d9d33e4d684753fd26cdafeaec
damiansarnecki <xdamiansarnecki@gmail.com>
2021-11-03 19:46:34 +0100

    added language button

--- src/components/NavBar/SelectLanguage/index.styles.tsx
+++ src/components/NavBar/SelectLanguage/index.styles.tsx
@@ -1,3 +1,31 @@
 import styled from "styled-components"
+import { isTypeAliasDeclaration } from "typescript";
 
-export const StyledSelectLanguage = styled.div``
+export const StyledSelectLanguage = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+`
+
+export const StyledLanguageIcon = styled.img``;
+
+export const StyledLanguageList = styled.select`
+    border: none;
+    color: white;
+    background-color: transparent;
+    font-family: Sora, sans-serif;
+    font-family: Sora;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 20px;
+    cursor: pointer;
+
+    :focus {
+        outline: none;
+    }
+`;
+
+export const StyledLanguageOption = styled.option`
+
+`;
