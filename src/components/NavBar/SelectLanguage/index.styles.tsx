c325c77eb96f4d33046dc6adf6fc8043add83d02
patrykrossa <patrros601@student.polsl.pl>
2021-11-04 23:50:30 +0100

    translation en/pl

--- src/components/NavBar/SelectLanguage/index.styles.tsx
+++ src/components/NavBar/SelectLanguage/index.styles.tsx
@@ -1,21 +1,13 @@
 import styled from "styled-components"
 import { isTypeAliasDeclaration } from "typescript"
+import { Swatches } from "../../../__styles__/Swatches"
 
-export const StyledSelectLanguage = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 10px;
-`
-
-export const StyledLanguageIcon = styled.img``
-
-export const StyledLanguageList = styled.select`
+/*export const StyledLanguageList = styled.select`
     appearance: none;
     border: none;
     color: white;
     background-color: transparent;
     font-family: Sora, sans-serif;
-    font-family: Sora;
     font-style: normal;
     font-weight: bold;
     font-size: 16px;
@@ -38,4 +30,54 @@ export const StyledLanguageList = styled.select`
 
 export const StyledLanguageOption = styled.option`
     background-color: black;
+`*/
+
+export const StyledSelectLanguage = styled.div`
+    display: flex;
+    gap: 10px;
+    justify-content: space-around;
+    align-items: center;
+    position: relatve;
+`
+
+export const StyledLanguageIcon = styled.img`
+    display: flex;
+`
+
+export const StyledLanguage = styled.div`
+    cursor: pointer;
+    text-transform: uppercase;
+    :hover {
+        opacity: 0.9;
+    }
+    display: flex;
+    gap: 10px;
+`
+
+export const StyledLanguageList = styled.ul`
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 6rem;
+    position: absolute;
+    margin-right: 2rem;
+`
+
+export const StyledLanguageOption = styled.button`
+    cursor: pointer;
+    font-family: Sora, sans-serif;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 16px;
+    background-color: black;
+    color: white;
+    padding: 0.2rem;
+    margin-top: 0.1rem;
+    border: solid 1.5px ${Swatches.primary_color};
+    border-radius: 5px;
+    width: 150%;
+
+    :hover {
+        opacity: 0.9;
+    }
 `
--- src/components/NavBar/SelectLanguage/index.tsx
+++ src/components/NavBar/SelectLanguage/index.tsx
@@ -1,24 +1,54 @@
+import { useState } from "react"
 import {
     StyledLanguageIcon,
     StyledLanguageList,
     StyledLanguageOption,
     StyledSelectLanguage,
+    StyledLanguage,
 } from "./index.styles"
 import i18n from "i18n"
 
 export const SelectLanguage = () => {
     const handleLanguageChange = (e: any) => {
         i18n.changeLanguage(e.target.value)
+        setLanguage(e.target.value)
+        setIsClicked(!isClicked)
     }
 
+    const handleClick = () => {
+        setIsClicked(!isClicked)
+    }
+
+    const [language, setLanguage] = useState("pl")
+    const [isClicked, setIsClicked] = useState(false)
+
     return (
-        <StyledSelectLanguage>
-            <StyledLanguageIcon src="/assets/icons/globe.svg" />
-            <StyledLanguageList onChange={handleLanguageChange}>
-                <StyledLanguageOption value="pl">PL</StyledLanguageOption>
-                <StyledLanguageOption value="en">EN</StyledLanguageOption>
-            </StyledLanguageList>
-            <StyledLanguageIcon src="/assets/icons/chevron-down.svg" />
-        </StyledSelectLanguage>
+        <>
+            {isClicked ? (
+                <StyledSelectLanguage>
+                    <StyledLanguageIcon src="/assets/icons/globe.svg" />
+                    <StyledLanguage onClick={handleClick}>
+                        {language}
+                        <StyledLanguageIcon src="/assets/icons/chevron-down.svg" />
+                    </StyledLanguage>
+                    <StyledLanguageList>
+                        <StyledLanguageOption value="pl" onClick={handleLanguageChange}>
+                            PL
+                        </StyledLanguageOption>
+                        <StyledLanguageOption value="en" onClick={handleLanguageChange}>
+                            EN
+                        </StyledLanguageOption>
+                    </StyledLanguageList>
+                </StyledSelectLanguage>
+            ) : (
+                <StyledSelectLanguage>
+                    <StyledLanguageIcon src="/assets/icons/globe.svg" />
+                    <StyledLanguage onClick={handleClick}>
+                        {language}
+                        <StyledLanguageIcon src="/assets/icons/chevron-down.svg" />
+                    </StyledLanguage>
+                </StyledSelectLanguage>
+            )}
+        </>
     )
 }
