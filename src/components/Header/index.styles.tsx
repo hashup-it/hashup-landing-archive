54bd0b308ddd9249f58a50f51cb4d3ea7da24255
EdwardHoper <21258391+EdwardHoper@users.noreply.github.com>
2021-12-16 18:31:44 +0100

    bg height fixes

--- src/components/Header/index.styles.tsx
+++ src/components/Header/index.styles.tsx
@@ -4,11 +4,14 @@ import styled from "styled-components"
 import { Swatches } from "__styles__/Swatches"
 import { mediaQuery } from "../MediaQuery"
 
+const minHeight: string = "900px"
+
 export const StyledInnerContainer = styled.div`
     grid-template-columns: 1fr 1fr;
     height: 100vh;
     position: relative;
     z-index: 999;
+    min-height: ${minHeight};
 
     ${mediaQuery.laptopM} {
         height: auto;
@@ -26,37 +29,37 @@ export const StyledContentBox = styled.div`
 export const StyledBackgroundContainer = styled.div`
     width: 100vw;
     height: 100vh;
+    min-height: ${minHeight};
     position: absolute;
     overflow-y: hidden;
     top: 0;
+    background-color: black;
     left: 0;
     z-index: 0;
+
+    ${mediaQuery.laptopM} {
+        display: none;
+    }
 `
 
 export const StyledBackgroundPlaceholderImg = styled.div<{ isVisible: boolean }>`
     opacity: ${props => (props.isVisible ? 1 : 0)};
     width: 100%;
     height: 100%;
+    min-height: ${minHeight};
     position: absolute;
     top: 0;
     left: 0;
     background-color: black;
     z-index: 2;
     transition: opacity 500ms ease;
-
-    ${mediaQuery.laptopM} {
-        display: none;
-    }
 `
 
 export const StyledVideoBackground = styled.video<{ isLoaded: boolean }>`
     opacity: ${props => (props.isLoaded ? 1 : 0)};
     width: 100%;
+    min-height: ${minHeight};
     z-index: 1;
-
-    ${mediaQuery.laptopM} {
-        display: none;
-    }
 `
 
 export const StyledBackgroundFlare = styled.div`
