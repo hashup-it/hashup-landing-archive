0f0cc90ca1dd62d260f4b3aa3eaad63d92236340
EdwardHoper <21258391+EdwardHoper@users.noreply.github.com>
2022-01-10 14:38:47 +0100

    config fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,4 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
     enabled: process.env.ANALYZE === "true",
 })
 
-module.exports = withBundleAnalyzer({
-    distDir: "out",
-})
+module.exports = withBundleAnalyzer()
